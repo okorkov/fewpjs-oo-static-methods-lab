6e33b7ba06e337ddb05bfbf21e340f7e1c110570
okorkov <alex.okorkov@gmail.com>
2021-02-22 13:11:21 -0800

    commit tree 3f2907a305516fc0d540a3d97119cbcb5a5cf123

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    const exceptionArray = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = string.split(' ');
+    let newWords = [];
+    for(let word of words) {
+      if (exceptionArray.indexOf(word) === -1) {
+        newWords.push(this.capitalize(word));
+      } else {
+        if (words.indexOf(word) === 0) {
+          newWords.push(this.capitalize(word));
+        } else {
+          newWords.push(word);
+        }
+      }
+    }
+    return newWords.join(' ');
+  }
 }
